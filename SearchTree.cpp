927784d78d32d354efc9e6afd0f994c770971951
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-06-11 11:38:40 +0700

    menambahkan case 5

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -174,6 +174,17 @@ int main()
             obj.postorder(obj.ROOT);
             break;
         }
+
+        case '5':
+            return 0;
+
+        default:
+        {
+            cout << "Invalid option" << endl;
+            break;
+        }
         }
     }
+
+    return 0;
 }
