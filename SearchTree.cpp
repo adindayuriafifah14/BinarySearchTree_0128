ce052c99497f3311e69079d152344a6ab3eff211
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-06-11 11:36:26 +0700

    menambahkan case 4

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -168,6 +168,12 @@ int main()
             obj.preorder(obj.ROOT);
             break;
         }
+
+        case '4':
+        {
+            obj.postorder(obj.ROOT);
+            break;
+        }
         }
     }
 }
